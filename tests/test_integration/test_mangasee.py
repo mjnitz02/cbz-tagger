2313a7fc9a00fa64a4f37cffb0aad6c5b407b046
Matt Nitzken <mjnitz02@gmail.com>
2024-10-10 16:49:26 -0400

    Initial concept for a mangasee retriever

--- tests/test_integration/test_mangasee.py
+++ tests/test_integration/test_mangasee.py
@@ -1,10 +1,8 @@
-from ast import parse
-
 from cbz_tagger.entities.mangasee import MangaSeeChapterEntity
 
 
 def test_mangasee_rss():
-    parser = MangaSeeChapterEntity()
+    parser = MangaSeeChapterEntity({})
     chapters = parser.parse_rss_feed("Tonikaku-Kawaii")
-    chapter_urls = parser.parse_mangasee_chapter_pages(chapters["Chapter 290"])
-    assert True
+    chapter_urls = parser.parse_chapter_download_links(chapters["Chapter 290"])
+    assert chapter_urls
