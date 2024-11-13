67fc20f8802b3ccae041eb67b969f4566542b8c6
Matt Nitzken <mjnitz02@gmail.com>
2024-11-13 14:15:31 -0500

    Fix timer not refreshing properly (#25)
    * Fix timer not refreshing properly
    
    * Fix logger tests

--- tests/test_unit/test_gui/test_functions.py
+++ tests/test_unit/test_gui/test_functions.py
@@ -39,6 +39,8 @@ def test_add_new_to_scanner():
 @patch("cbz_tagger.gui.functions.logger")
 def test_notify_and_log(mock_logger, mock_ui_notify):
     msg = "Test message"
-    notify_and_log(msg)
+    with patch("cbz_tagger.gui.functions.datetime") as mock_datetime:
+        mock_datetime.now.return_value = "test_date"
+        notify_and_log(msg)
     mock_ui_notify.assert_called_once_with(msg)
-    mock_logger.info.assert_called_once_with(msg)
+    mock_logger.info.assert_called_once_with("%s %s", "test_date", msg)
