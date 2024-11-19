2f3d1b8af8ce259f1c601e679a65bc075e338339
Matt Nitzken <mjnitz02@gmail.com>
2024-11-19 15:45:56 -0500

    Remove header

--- cbz_tagger/entities/base_entity.py
+++ cbz_tagger/entities/base_entity.py
@@ -18,10 +18,6 @@ logger = logging.getLogger()
 
 class BaseEntityObject:
     base_url = f"https://api.{Urls.MDX}"
-    base_header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/131.0.0.0 Safari/537.36"
-    }
 
 
 class BaseEntity(BaseEntityObject):
