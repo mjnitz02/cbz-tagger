a226ae450504a74f50c24bb9322cdc08b038e88e
Matt Nitzken <mjnitz02@gmail.com>
2025-08-26 16:22:27 -0400

    Linter

--- cbz_tagger/common/version.py
+++ cbz_tagger/common/version.py
@@ -8,10 +8,8 @@ def extract_version() -> str:
     """Returns either the version of installed package or the one
     found in nearby pyproject.toml"""
     with suppress(FileNotFoundError, StopIteration):
-        with open(
-            (root_dir := Path(__file__).parent.parent.parent) / "pyproject.toml", encoding="utf-8"
-        ) as pyproject_toml:
-            lines = [line for line in pyproject_toml]
+        with open(Path(__file__).parent.parent.parent / "pyproject.toml", encoding="utf-8") as pyproject_toml:
+            lines = list(line for line in pyproject_toml)
             version = next(line for line in lines if line.startswith("version = ")).split("=")[1].strip("'\"\n ")
             return f"{version}"
     return f"{APPLICATION_MAJOR_VERSION}"
