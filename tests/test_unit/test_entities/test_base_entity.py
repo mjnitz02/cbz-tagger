a6c282449e11083d4c92c1eefded19184b74266f
Matt Nitzken <mjnitz02@gmail.com>
2026-01-08 14:04:31 -0500

    Fix tests

--- tests/test_unit/test_entities/test_base_entity.py
+++ tests/test_unit/test_entities/test_base_entity.py
@@ -60,7 +60,7 @@ def test_base_entity_to_hash(manga_request_content):
     assert entity4.to_hash() == hash_value
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_request_with_retry_success(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get("http://example.com/file", json={"data": "file content"})
@@ -69,10 +69,10 @@ def test_request_with_retry_success(mock_sleep):
 
         assert result.status_code == 200
         assert result.json() == {"data": "file content"}
-        mock_sleep.assert_called_once_with(0.3)
+        mock_sleep.assert_called_once_with(0.5)
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_request_with_retry_retry_success(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get(
@@ -89,13 +89,13 @@ def test_request_with_retry_retry_success(mock_sleep):
         # Should have 1 retry of 5s sleep, and one default sleep of 0.3 on success
         mock_sleep.assert_has_calls(
             [
-                mock.call(5),
-                mock.call(0.3),
+                mock.call(10),
+                mock.call(0.5),
             ]
         )
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_request_with_retry_timeout(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get("http://example.com/file", exc=requests.exceptions.ConnectTimeout)
@@ -108,14 +108,14 @@ def test_request_with_retry_timeout(mock_sleep):
         # Assert has 3 retry sleeps of increasing time
         mock_sleep.assert_has_calls(
             [
-                mock.call(5),
                 mock.call(10),
-                mock.call(15),
+                mock.call(20),
+                mock.call(30),
             ]
         )
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_request_with_retry_failure(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get("http://example.com/file", json={"data": "file content"}, status_code=500)
@@ -128,14 +128,14 @@ def test_request_with_retry_failure(mock_sleep):
         # Assert has 3 retry sleeps of increasing time
         mock_sleep.assert_has_calls(
             [
-                mock.call(5),
                 mock.call(10),
-                mock.call(15),
+                mock.call(20),
+                mock.call(30),
             ]
         )
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 @patch("cbz_tagger.entities.base_entity.AppEnv")
 def test_request_with_retry_with_proxy(mock_app_env, mock_sleep):
     def verify_proxy_in_headers(request, content):
@@ -151,10 +151,10 @@ def test_request_with_retry_with_proxy(mock_app_env, mock_sleep):
 
         assert result.status_code == 200
         assert result.text == "passed"
-        mock_sleep.assert_called_once_with(0.3)
+        mock_sleep.assert_called_once()
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_download_file_success(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get("http://example.com/file", content=b"file content")
@@ -162,10 +162,10 @@ def test_download_file_success(mock_sleep):
         result = BaseEntity.download_file("http://example.com/file")
 
         assert result == b"file content"
-        mock_sleep.assert_called_once()
+        mock_sleep.assert_called_once_with(0.5)
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_download_file_retry_success(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get("http://example.com/file", [{"status_code": 500}, {"content": b"file content"}])
@@ -174,10 +174,10 @@ def test_download_file_retry_success(mock_sleep):
 
         assert result == b"file content"
         # assert mock_requests_get.call_count == 2
-        mock_sleep.assert_called_with(0.3)
+        mock_sleep.assert_called_with(0.5)
 
 
-@patch("cbz_tagger.entities.base_entity.sleep")
+@patch("cbz_tagger.entities.base_entity.time.sleep")
 def test_download_file_failure(mock_sleep):
     with requests_mock.Mocker() as rm:
         rm.get("http://example.com/file", status_code=500)
