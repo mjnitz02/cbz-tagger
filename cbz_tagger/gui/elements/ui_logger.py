de254421b6b62dfa9a2b89e952b43334fef9c030
Matt Nitzken <mjnitz02@gmail.com>
2026-01-12 12:08:47 -0500

    Changes

--- cbz_tagger/gui/elements/ui_logger.py
+++ cbz_tagger/gui/elements/ui_logger.py
@@ -11,7 +11,8 @@ logger = logging.getLogger()
 def ui_logger():
     env = AppEnv()
     log = ui.log(max_lines=1000).classes("w-full").style("height: 70vh")
-    handler = LogElementHandler(log, level=env.LOG_LEVEL)
-    logger.addHandler(handler)
+    handler = LogElementHandler(log)
+    # logger.addHandler(handler)
+    ui.context.client.on_disconnect(lambda: logger.removeHandler(handler))
 
     return log
