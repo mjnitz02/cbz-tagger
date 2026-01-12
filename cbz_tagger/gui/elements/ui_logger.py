22567dacd8ce323cb528b347d04da8a54e69e32f
Matt Nitzken <mjnitz02@gmail.com>
2026-01-12 12:09:30 -0500

    Undo logger

--- cbz_tagger/gui/elements/ui_logger.py
+++ cbz_tagger/gui/elements/ui_logger.py
@@ -11,8 +11,7 @@ logger = logging.getLogger()
 def ui_logger():
     env = AppEnv()
     log = ui.log(max_lines=1000).classes("w-full").style("height: 70vh")
-    handler = LogElementHandler(log)
-    # logger.addHandler(handler)
-    ui.context.client.on_disconnect(lambda: logger.removeHandler(handler))
+    handler = LogElementHandler(log, level=env.LOG_LEVEL)
+    logger.addHandler(handler)
 
     return log
