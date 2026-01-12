2861204cc82e25bdfcb45e51a5bf0acf1099ee54
Matt Nitzken <mjnitz02@gmail.com>
2026-01-12 12:21:26 -0500

    Remove zip encoding problems

--- cbz_tagger/entities/base_entity.py
+++ cbz_tagger/entities/base_entity.py
@@ -78,7 +78,6 @@ class BaseEntity(BaseEntityObject):
                 "headers": {
                     "Accept": "application/json,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                     "Accept-Language": "en-US,en;q=0.9",
-                    "Accept-Encoding": "gzip, deflate, br",
                     "DNT": "1",
                     "Connection": "keep-alive",
                     "Upgrade-Insecure-Requests": "1",
@@ -90,7 +89,6 @@ class BaseEntity(BaseEntityObject):
                 "headers": {
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                     "Accept-Language": "en-US,en;q=0.5",
-                    "Accept-Encoding": "gzip, deflate, br",
                     "DNT": "1",
                     "Connection": "keep-alive",
                 },
@@ -101,7 +99,6 @@ class BaseEntity(BaseEntityObject):
                 "headers": {
                     "Accept": "application/json,text/html,application/xhtml+xml",
                     "Accept-Language": "en-GB,en;q=0.9",
-                    "Accept-Encoding": "gzip, deflate, br",
                     "Connection": "keep-alive",
                     "Sec-Fetch-Dest": "document",
                     "Sec-Fetch-Mode": "navigate",
