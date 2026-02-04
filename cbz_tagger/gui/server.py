0eacd5c094fa3b70d161c56fa3cec2fc38658c6d
Matt Nitzken <mjnitz02@gmail.com>
2026-02-04 10:30:37 -0500

    Fix

--- cbz_tagger/gui/server.py
+++ cbz_tagger/gui/server.py
@@ -6,7 +6,6 @@ import os
 from nicegui import app
 from nicegui import ui
 
-from cbz_tagger.common.env import AppEnv
 from cbz_tagger.gui.gui import SimpleGui
 
 logger = logging.getLogger(__name__)
@@ -14,11 +13,11 @@ logger = logging.getLogger(__name__)
 
 def main():
     """Run the FastAPI server."""
-    env = AppEnv()
+    # env = AppEnv()
 
     # Configure logging
 
-    logging.basicConfig(level=env.LOG_LEVEL)
+    logging.basicConfig(level=logging.INFO)
 
     logger.info("Starting CBZ Tagger NiceGUI server...")
 
@@ -27,7 +26,7 @@ def main():
     root_path = os.path.dirname(os.path.abspath(__file__))
     static_path = os.path.join(root_path, "static")
     app.add_static_files("/static", static_path)
-    ui.run(reload=env.DEBUG_MODE, favicon=os.path.join(static_path, "favicon.ico"))
+    ui.run(reload=False, favicon=os.path.join(static_path, "favicon.ico"))
 
 
 if __name__ in {"__main__", "__mp_main__"}:
