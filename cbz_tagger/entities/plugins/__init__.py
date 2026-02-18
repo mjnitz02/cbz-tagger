75c48cee49298c43be44933ee161ff22d92cbdab
Matt Nitzken <mjnitz02@gmail.com>
2026-02-18 16:34:31 -0500

    Remove CMK plugin entirely

--- cbz_tagger/entities/plugins/__init__.py
+++ cbz_tagger/entities/plugins/__init__.py
@@ -1,11 +1,9 @@
 # Import all plugins to trigger registration with @Plugins.register()
-from cbz_tagger.entities.plugins.cmk import ChapterPluginCMK
 from cbz_tagger.entities.plugins.kal import ChapterPluginKAL
 from cbz_tagger.entities.plugins.mdx import ChapterPluginMDX
 from cbz_tagger.entities.plugins.wbc import ChapterPluginWBC
 
 __all__ = [
-    "ChapterPluginCMK",
     "ChapterPluginKAL",
     "ChapterPluginMDX",
     "ChapterPluginWBC",
