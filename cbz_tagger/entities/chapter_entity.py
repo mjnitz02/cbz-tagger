59d4d0c0c57aaca7c729d3b2d0b0c842d4e7a6ae
Matt Nitzken <mjnitz02@gmail.com>
2026-02-18 13:57:04 -0500

    Remove imports

--- cbz_tagger/entities/chapter_entity.py
+++ cbz_tagger/entities/chapter_entity.py
@@ -1,10 +1,5 @@
 import logging
-import os
-from io import BytesIO
 from typing import Any
-
-from PIL import Image
-from PIL import ImageFile
 
 # Import plugins to trigger registration
 import cbz_tagger.entities.chapter_plugins  # noqa: F401
@@ -51,33 +46,3 @@ class ChapterEntity(ChapterPluginEntity):
 
     def parse_chapter_download_links(self, url: str) -> list[str]:
         return self.entity_plugin.parse_chapter_download_links(url)
-
-    def download_chapter(self, filepath) -> list[str]:
-        # Get chapter image urls
-        url = self.get_chapter_url()
-        download_links = self.parse_chapter_download_links(url)
-
-        # Download the images for the chapter
-        cached_images = []
-        for index, image_url in enumerate(download_links):
-            image_path = os.path.join(filepath, f"{index + 1:03}.jpg")
-            cached_images.append(image_path)
-            if not os.path.exists(image_path):
-                image = self.download_file(image_url)
-                in_memory_image = Image.open(BytesIO(image))
-                if in_memory_image.format != "JPEG":
-                    in_memory_image = in_memory_image.convert("RGB")
-                try:
-                    in_memory_image.save(image_path, quality=95, optimize=True)
-                except OSError:
-                    ImageFile.LOAD_TRUNCATED_IMAGES = True  # type: ignore[misc]
-                    in_memory_image.save(image_path, quality=95, optimize=True)
-
-        if self.pages != -1 and len(cached_images) != self.pages:
-            logger.error("Failed to download chapter %s, not enough pages saved from server", self.entity_id)
-            raise EnvironmentError(f"Failed to download chapter {self.entity_id}, not enough pages saved from server")
-        if self.pages == -1 and len(cached_images) != len(download_links):
-            logger.error("Failed to download chapter %s, not enough pages saved from server", self.entity_id)
-            raise EnvironmentError(f"Failed to download chapter {self.entity_id}, not enough pages saved from server")
-
-        return cached_images
