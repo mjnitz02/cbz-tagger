cf7e277e3aa57fc0ad662d05f8cf882e124b04df
Matt Nitzken <mjnitz02@gmail.com>
2026-05-07 15:36:59 -0400

    Allow webp

--- tests/test_integration/test_chapter_plugins_api.py
+++ tests/test_integration/test_chapter_plugins_api.py
@@ -14,7 +14,7 @@ def check_entity_download_links(entity, entity_link_count):
     response = entity.request_with_retry(download_links[0])
     assert response.status_code == 200
     in_memory_image = Image.open(BytesIO(response.content))
-    assert in_memory_image.format in ["JPEG", "PNG"]
+    assert in_memory_image.format in ["JPEG", "PNG", "WEBP"]
 
 
 # These are random selections of cancelled free web comics for testing the API connections
