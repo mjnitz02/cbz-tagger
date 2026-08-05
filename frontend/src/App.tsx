4ce9c0cfda1f774ed92a6cd2a0335c9b02c98d72
Matt Nitzken <mjnitz02@gmail.com>
2026-08-05 12:05:47 -0400

    Fix cves

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import AppLayout from './components/AppLayout'
 import SeriesPage from './pages/SeriesPage'
 import AddSeriesPage from './pages/AddSeriesPage'
